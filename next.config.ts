799fc44bb6aa599a3a45a5dcd87c8d0b259e2f10
Arush Nandakumar <arushn.2005@gmail.com>
2026-09-14 01:40:17 +0530

    feat: redirect wheresmyoffer.vercel.app to www.wheresmyoffer.in

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'wheresmyoffer.vercel.app',
+          },
+        ],
+        destination: 'https://www.wheresmyoffer.in/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
+
